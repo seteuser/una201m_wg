0ed21db505b2d5d7976fda0e2809f307c5a33768
amsilva <andre.marcos@gmail.com>
2020-04-25 15:43:15 +0000

    estrutura basica, estrutura

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main{
+
+   
+  public static void main(final String[] args) {
+
+    System.out.println("iniciando...");
+        
+    //MecanicaDoJogo mecanica = FabricaMecanicaDoJogo.nova();
+
+
+    System.out.println("fim, ok!");
+        
+
+    }
+}
