8d7b92580716363e5467bb80edde46f8a3ee637d
andre.marcos <amsilva@gmail.com>
2020-04-25 23:16:49 -0300

    Incluindo oDAO

---
+++ PalavraDAO.java
@@ -0,0 +1,29 @@
+
+import java.io.RandomAccessFile;
+import java.io.IOException;
+
+public class PalavraDAO {
+
+      //public ArrayList<Palavra> selectAll() {
+  //     return new ArrayList<Palavra>;
+  //}
+
+  //public String selectFirst() {
+  //  return "";
+  //}
+
+  //public String selectLast() {
+  //  return "";
+  //}
+
+  //public ArrayList<Palavra> selectBySize() {
+  //  return new ArrayList<Palavra>;
+  //}
+
+  //public ArrayList<Palavra> selectByCategory() {
+  //  return new ArrayList<Palavra>;
+  //}
+
+  
+
+}
